945089495dcc25c69ca5fb961c8a33a9910ec159
Alfaquillo <79667145+alfaquillo@users.noreply.github.com>
2025-05-19 20:22:59 -0600

    correcciones
    falta el top

--- Interfaz_de_memoria/Interfaz_de_memoria.srcs/sources_1/imports/BancoInterfaz/queue.v
+++ Interfaz_de_memoria/Interfaz_de_memoria.srcs/sources_1/imports/BancoInterfaz/queue.v
@@ -9,10 +9,10 @@ module queue (CLK, RST, ENA, DATAIN, DATAOUT);
     // DATAIN -> R1 -> R2 -> R3 -> R4
     wire [7:0] DOUT1, DOUT2, DOUT3, DOUT4;
 
-    Registro8Ena R1(CLK, RST, ENA, DATAIN, DOUT1);
-    Registro8Ena R2(CLK, RST, ENA, DOUT1, DOUT2);
-    Registro8Ena R3(CLK, RST, ENA, DOUT2, DOUT3);
-    Registro8Ena R4(CLK, RST, ENA, DOUT3, DOUT4);
+    Registro8bitsEna R1(CLK, RST, ENA, DATAIN, DOUT1);
+    Registro8bitsEna R2(CLK, RST, ENA, DOUT1, DOUT2);
+    Registro8bitsEna R3(CLK, RST, ENA, DOUT2, DOUT3);
+    Registro8bitsEna R4(CLK, RST, ENA, DOUT3, DOUT4);
 
     assign DATAOUT = {DOUT4, DOUT3, DOUT2, DOUT1};
 
